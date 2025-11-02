046e403d3ca2887e6f02a7fc5004bc36a03862ab
SimonWJohnson <simonwjohnson@gmail.com>
2025-11-02 21:17:15 +1030

    Added React functionality for Play and Stop buttons

--- src/components/DJ_Controls.jsx
+++ src/components/DJ_Controls.jsx
@@ -6,34 +6,34 @@ function DJ_Controls() {
                 <input type="text" class="form-control" id="cpm_text-input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label" />
             </div>
             <br />
-            <label for="volume_range" className="form-label">Volume</label>
+            <label htmlFor="volume_range" className="form-label">Volume</label>
             <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range" />
 
-            <label for="reverb_range" className="form-label">Reverb</label>
+            <label htmlFor="reverb_range" className="form-label">Reverb</label>
             <input type="range" className="form-range" min="0" max="1" step="0.01" id="reverb_range" />
             <br />
             <br />
             <div class="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="inst1"/>
-                    <label className="form-check-label" for="checkDefault">
+                <label className="form-check-label" htmlFor="checkDefault">
                         Instrument 1
                     </label>
             </div>
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="inst2" />
-                    <label className="form-check-label" for="checkChecked">
+                <label className="form-check-label" htmlFor="checkChecked">
                     Instrument 2
                     </label>
             </div>
             <div className="form-check">
                 <input className="form-check-input" type="checkbox" value="" id="inst3" />
-                <label className="form-check-label" for="checkChecked">
+                <label className="form-check-label" htmlFor="checkChecked">
                     Instrument 3
                 </label>
             </div>
             <br />
             <div className="mb-3">
-                <label className="form-label d-block" for="save_settings">Save or Load Settings</label>
+                <label className="form-label d-block" htmlFor="save_settings">Save or Load Settings</label>
                 <div className="btn-group" role="group" aria-label="Save/Load">
                     <button id="save_settings" className="btn btn-outline-success">Save</button>
                     <button id="load_settings" className="btn btn-outline-danger">Load</button>
--- src/components/PlayButtons.jsx
+++ src/components/PlayButtons.jsx
@@ -1,9 +1,9 @@
-function PlayButtons() {
+function PlayButtons({ onPlay, onStop}) {
   return (
       <>
-          <div className="btn-group" role="group" atia-label="Basic mixed styles example">
-              <button id="play" className="btn btn-outline-success">Play</button>
-              <button id="stop" className="btn btn-outline-danger">Stop</button>
+          <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+              <button id="play" className="btn btn-outline-success" onClick={onPlay}>Play</button>
+              <button id="stop" className="btn btn-outline-danger" onClick={onStop}>Stop</button>
           </div>
       </>
   );
