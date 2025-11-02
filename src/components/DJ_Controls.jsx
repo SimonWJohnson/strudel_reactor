efca0c9d6323ea064909e2f8961275ba48a21481
SimonWJohnson <simonwjohnson@gmail.com>
2025-11-02 20:23:22 +1030

    Added features to the DJ_Controls - sliders, buttons, radio buttons for future functionality

--- src/components/DJ_Controls.jsx
+++ src/components/DJ_Controls.jsx
@@ -1,20 +1,47 @@
 function DJ_Controls() {
     return (
         <>
-            <div className="form-check">
-                {/*<input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" onChange={ProcAndPlay} defaultChecked />*/}
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
-                <label className="form-check-label" htmlFor="flexRadioDefault1">
-                    p1: ON
-                </label>
+            <div className="input-group mb-3">
+                <span className="input-group-text" id="cpm_label">setCPM</span>
+                <input type="text" class="form-control" id="cpm_text-input" placeholder="120" aria-label="cpm" aria-describedby="cpm_label" />
+            </div>
+            <br />
+            <label for="volume_range" className="form-label">Volume</label>
+            <input type="range" className="form-range" min="0" max="1" step="0.01" id="volume_range" />
+
+            <label for="reverb_range" className="form-label">Reverb</label>
+            <input type="range" className="form-range" min="0" max="1" step="0.01" id="reverb_range" />
+            <br />
+            <br />
+            <div class="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="inst1"/>
+                    <label className="form-check-label" for="checkDefault">
+                        Instrument 1
+                    </label>
             </div>
             <div className="form-check">
-                {/*<input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" onChange={ProcAndPlay} />*/}
-                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                <label className="form-check-label" htmlFor="flexRadioDefault2">
-                    p1: HUSH
+                <input className="form-check-input" type="checkbox" value="" id="inst2" />
+                    <label className="form-check-label" for="checkChecked">
+                    Instrument 2
+                    </label>
+            </div>
+            <div className="form-check">
+                <input className="form-check-input" type="checkbox" value="" id="inst3" />
+                <label className="form-check-label" for="checkChecked">
+                    Instrument 3
                 </label>
             </div>
+            <br />
+            <div className="mb-3">
+                <label className="form-label d-block" for="save_settings">Save or Load Settings</label>
+                <div className="btn-group" role="group" aria-label="Save/Load">
+                    <button id="save_settings" className="btn btn-outline-success">Save</button>
+                    <button id="load_settings" className="btn btn-outline-danger">Load</button>
+                </div>
+            </div>
+
+
+
         </>
 
   );
