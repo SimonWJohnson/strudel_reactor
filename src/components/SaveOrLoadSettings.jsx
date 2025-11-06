5ff08fc47fa5871e6830929be5129205dfbde495
SimonWJohnson <simonwjohnson@gmail.com>
2025-11-06 19:19:03 +1030

    Centred Save and Load buttons in the DJ Panel

--- src/components/SaveOrLoadSettings.jsx
+++ src/components/SaveOrLoadSettings.jsx
@@ -1,7 +1,7 @@
 function SaveOrLoadSettings() {
     return (
         <>
-            <div className="mb-3">
+            <div className="d-flex flex-column align-items-center text-center gap-3">
                 <label className="form-label d-block" htmlFor="save_settings">Save or Load Settings</label>
                 <div className="btn-group" role="group" aria-label="Save/Load">
                     <button id="save_settings" className="btn btn-outline-success">Save</button>
