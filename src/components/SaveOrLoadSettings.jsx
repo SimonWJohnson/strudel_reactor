85469bc4aa533ac257f08224298b506da6fd1138
SimonWJohnson <simonwjohnson@gmail.com>
2025-11-16 00:35:26 +1030

    Small but cool css / styling tweak

--- src/components/SaveOrLoadSettings.jsx
+++ src/components/SaveOrLoadSettings.jsx
@@ -4,7 +4,12 @@ function SaveOrLoadSettings() {
     return (
         <>
             <div className="d-flex flex-column align-items-center text-center gap-3">
-                <label className="form-label d-block" htmlFor="save_settings">Save or Load Settings</label>
+                {/*<label className="form-label d-block" htmlFor="save_settings">Save or Load Settings</label>*/}
+                <div className="mb-2 text-center">
+                    <h5 className="m-0 dj-title" style={{ fontWeight: "600", letterSpacing: "0.5px" }}>
+                        Save and Load Settings
+                    </h5>
+                </div>
                 <div className="btn-group" role="group" aria-label="Save/Load">
                     <button id="save_settings" className="btn btn-outline-success">Save</button>
                     <button id="load_settings" className="btn btn-outline-danger">Load</button>
