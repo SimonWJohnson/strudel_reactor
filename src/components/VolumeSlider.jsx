8ab2ed58d678c634a72307fe49af549826c41cb8
SimonWJohnson <simonwjohnson@gmail.com>
2025-11-16 18:02:33 +1030

    Still working on refactoring...will come back to this

--- src/components/VolumeSlider.jsx
+++ src/components/VolumeSlider.jsx
@@ -1,10 +1,22 @@
-
+import { useEffect, useState } from "react";
 function VolumeSlider({ volume, onVolumeChange }) {
     // Debugging logic
+
+    // local Ui value for slider 
+    const [uiValue, setUiValue] = useState(volume);
+
+    // keep the UI in sync if parent changes (load json)
+    useEffect(() => {
+        setUiValue(volume);
+    }, [volume]);
+
     const handleMouseUp = (e) => {
-        const value = parseFloat(e.target.value);
-        console.log("VolumeSlider: ", value);
-        onVolumeChange(value); 
+        //const value = parseFloat(e.target.value);
+        const uiValue = parseFloat(e.target.uiValue);
+        //console.log("VolumeSlider: ", value);
+        console.log("VolumeSlider: ", uiValue);
+        //onVolumeChange(value); 
+        onVolumeChange(uiValue); 
     }
 
 
@@ -17,6 +29,7 @@ function VolumeSlider({ volume, onVolumeChange }) {
                 min="0" max="2" step="0.01"
                 onMouseUp={handleMouseUp}
                 id="volume_range"
+                value={uiValue}
             />
         </>
     );
