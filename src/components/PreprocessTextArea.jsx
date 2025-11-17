f3327be46e5cb64c0128d0af64b34e484236fe8c
SimonWJohnson <simonwjohnson@gmail.com>
2025-11-17 20:05:58 +1030

    Added Assets folder to house images to underlay the preprocess text area

--- src/components/PreprocessTextArea.jsx
+++ src/components/PreprocessTextArea.jsx
@@ -4,7 +4,13 @@ function PreprocessTextArea({defaultValue, onChange }) {
           <div className="dj-subtitle-zone">
               <label htmlFor="exampleFormControlTextarea1" className="h3 m-0 dj-subtitle">Drop the beats here son...</label>
           </div>          
-          <textarea className="form-control" rows="15" defaultValue={defaultValue} onChange={onChange} id="proc" ></textarea>
+          <textarea
+              className="form-control"
+              rows="15"
+              defaultValue={defaultValue}
+              onChange={onChange}
+              id="proc" >
+          </textarea>
       </>
   );
 }
