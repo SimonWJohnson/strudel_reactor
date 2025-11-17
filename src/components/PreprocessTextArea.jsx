d01fa1d83d63bfb0b51070b98f837017655d1693
SimonWJohnson <simonwjohnson@gmail.com>
2025-11-17 21:46:59 +1030

    added css styling for the preprocessTextArea

--- src/components/PreprocessTextArea.jsx
+++ src/components/PreprocessTextArea.jsx
@@ -1,15 +1,23 @@
-function PreprocessTextArea({defaultValue, onChange }) {
+import sunsetSynth from '../Assets/sunsetSynth.jpg';
+
+function PreprocessTextArea({ defaultValue, onChange }) {
   return (
       <>
           <div className="dj-subtitle-zone">
-              <label htmlFor="exampleFormControlTextarea1" className="h3 m-0 dj-subtitle">Drop the beats here son...</label>
+              <label htmlFor="exampleFormControlTextarea1"
+                  className="h3 m-0 dj-subtitle">
+                  Drop the beats here son...
+              </label>
           </div>          
           <textarea
-              className="form-control"
+              /*className="form-control"*/
+              className="form-control dj-textarea"
               rows="15"
               defaultValue={defaultValue}
               onChange={onChange}
-              id="proc" >
+              id="proc"
+              style={{ "--bg-url": `url(${sunsetSynth})` }}
+          >
           </textarea>
       </>
   );
